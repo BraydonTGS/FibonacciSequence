769d36cc98f5c074057a9254aab4b5a828d0b7ef
Braydon Sutherland <BraydonSutherland@gmail.com>
2022-09-08 18:43:39 -0500

    basic console app for fib sequence

--- FibonacciSequence/Program.cs
+++ FibonacciSequence/Program.cs
@@ -1,3 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ static void nthFibonacci(int nthTerm)
+{
+    int number1 = 0;
+    int number2 = 1;
+    int nthNumber;
+
+    if(nthTerm == 0)
+    {
+        Console.WriteLine("Term #0: 0");
+    }
+    else if (nthTerm == 1)
+    {
+        Console.WriteLine("Term #1: 1"); 
+    }
+    else
+    {
+        for(int i = 0; i < nthTerm; i++)
+        {
+            Console.WriteLine("Term #" + i + ": " + number1);
+            nthNumber = number1 + number2;
+            number1 = number2;
+            number2 = nthNumber; 
+        }
+    }
+}
+
+while (true)
+{
+
+    try
+    {
+        // Prompt the user //
+        Console.Write("Please Enter a number to calculate: ");
+        int nthNumber = int.Parse(Console.ReadLine());
+        // Calculate Fib //
+        nthFibonacci(nthNumber); 
+    }
+
+    catch
+    {
+        Console.WriteLine("Invalid Number. Please Try Again"); 
+    }
+}
 
